e003080534f782ae5bae41a9cb35ceb7e2049f07
Roger Heinrich <rheinrich@navex.com>
2023-08-25 07:04:01 -0700

    Added an api

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxNTcxNA/spectral.js?branch_slug=master&token=3e5f7ead-41ea-4ecf-b574-173296b54b06";
+export default { extends: ruleset };
